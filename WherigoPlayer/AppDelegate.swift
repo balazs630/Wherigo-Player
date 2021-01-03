33bb5e1ea6a73551d96ec4f8d7436b0194593e32
Horváth Balázs <balazs630@icloud.com>
2021-01-03 01:02:34 +0100

    Add base iOS Xcode project

---
+++ WherigoPlayer/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  WherigoPlayer
+//
+//  Created by Balázs Horváth on 2021. 01. 03..
+//
+
+import UIKit
+
+@main class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ WherigoPlayer/AppFlow/MainScreen/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  WherigoPlayer
+//
+//  Created by Balázs Horváth on 2021. 01. 03..
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
