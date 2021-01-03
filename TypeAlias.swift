738ff7a670a7a81e34160fae31d66e5e59b04234
Horváth Balázs <balazs630@icloud.com>
2021-01-03 01:56:06 +0100

    Add openwig and kahlua java sources for j2objc transpile

---
+++ TypeAlias.swift
@@ -0,0 +1,12 @@
+//
+//  TypeAlias.swift
+//  WhereIGoPlayer
+//
+//  Created by Balázs Horváth on 2020. 12. 30..
+//
+
+typealias WIGFile = JavaIoFile
+typealias WIGFileOutputStream = JavaIoFileOutputStream
+
+typealias WIGSeekableFile = WIGWSeekableFile
+typealias WIGSaveFile = WIGWSaveFile
