b71fd29ee160ab1bbedf096a1c00d96b85d37618
Horváth Balázs <balazs630@icloud.com>
2021-01-03 01:26:17 +0100

    Add LocationService class

---
+++ WherigoPlayer/Services/LocationService.swift
@@ -0,0 +1,74 @@
+//
+//  LocationService.swift
+//  WhereIGoPlayer
+//
+//  Created by Balázs Horváth on 2018. 11. 03..
+//
+
+import CoreLocation
+
+class LocationService: NSObject {
+    // MARK: Properties
+    static let shared = LocationService()
+    private var locationManager: CLLocationManager
+
+    var location: CLLocation? {
+        return locationManager.location
+    }
+
+    var coordinates: CLLocationCoordinate2D? {
+        return locationManager.location?.coordinate
+    }
+
+    // MARK: Initializers
+    private override init() {
+        locationManager = CLLocationManager()
+        super.init()
+    }
+}
+
+// MARK: Utility methods
+extension LocationService {
+    func startTracking() {
+        requestLocationPermission()
+        locationManager.startUpdatingLocation()
+    }
+
+    private func requestLocationPermission() {
+        if !isAccessGranted() {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+
+    private func isAccessGranted() -> Bool {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return true
+        case .notDetermined, .restricted, .denied:
+            return false
+        @unknown default:
+            return false
+        }
+    }
+}
+
+// MARK: CLLocationManagerDelegate methods
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        debugPrint(error)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        case .restricted, .denied:
+            locationManager.stopUpdatingLocation()
+        case .notDetermined, .authorizedAlways:
+            break
+        @unknown default:
+            break
+        }
+    }
+}
