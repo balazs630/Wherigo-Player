4a9bddf4668075d3e6d5e0acfc15c302c0312f12
Horváth Balázs <balazs630@icloud.com>
2021-02-16 00:36:38 +0100

    Add file handling utils to CartridgeService

--- WherigoPlayer/Services/CartridgeService.swift
+++ WherigoPlayer/Services/CartridgeService.swift
@@ -13,8 +13,38 @@ class CartridgeService {
             let seekableFile = WIGSeekableFile(javaIoFile: $0)
             let saveFile = WIGSaveFile(javaIoFile: $0)
 
-            return WIGCartridgeFile.read(with: seekableFile, with: saveFile)
+            let cartridgeFile = WIGCartridgeFile.read(with: seekableFile, with: saveFile)
+            if let cartridgePath = $0.value(forKey: "path_") as? String {
+                cartridgeFile?.setValue(cartridgePath, forKey: "filename_")
+            }
+
+            return cartridgeFile
         }
+    }
+
+    static func copyLuaStdLibIfNecessary() {
+        guard let documentsStdLibPath = try? FileManager.default
+            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent(Constant.stdLibFileName)
+            .appendingPathExtension(Constant.stdLibFileExtension)
+            .path
+        else {
+            return
+        }
+
+        if !FileManager.default.fileExists(atPath: documentsStdLibPath) {
+            let sourceStdLibPath = Bundle.main.path(
+                forResource: Constant.stdLibFileName,
+                ofType: Constant.stdLibFileExtension
+            )!
+
+            try? FileManager.default.copyItem(atPath: sourceStdLibPath, toPath: documentsStdLibPath)
+        }
+    }
+
+    static func createLogOutputStream(for cartridgeFile: WIGCartridgeFile) -> WIGFileOutputStream {
+        let owlFile = WIGFile(nsString: createOwlFileIfNecessary(for: cartridgeFile))
+        return WIGFileOutputStream(javaIoFile: owlFile)
     }
 }
 
@@ -36,5 +66,33 @@ extension CartridgeService {
             Log.debug(error)
             return []
         }
+    }
+
+    private static func createOwlFileIfNecessary(for cartridgeFile: WIGCartridgeFile) -> String? {
+        guard let cartridgeFileName = cartridgeFileName(from: cartridgeFile),
+              let documentsOWLPath = try? FileManager.default
+                .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                .appendingPathComponent(cartridgeFileName)
+                .appendingPathExtension(Constant.owlFileExtension)
+                .path
+        else {
+            return nil
+        }
+
+        if !FileManager.default.fileExists(atPath: documentsOWLPath) {
+            FileManager.default.createFile(atPath: documentsOWLPath, contents: nil)
+        }
+
+        return documentsOWLPath
+    }
+
+    private static func cartridgeFileName(from cartridgeFile: WIGCartridgeFile) -> String? {
+        guard let cartridgeFileAbsolutePath = cartridgeFile.value(forKey: "filename_") as? String,
+              let lastPathComponent = URL(string: cartridgeFileAbsolutePath)?.lastPathComponent
+        else {
+            return nil
+        }
+
+        return lastPathComponent
     }
 }
